6bbce31b8a3f49716b74c8b78f4a84b112e886a5
Gusvarov <nazikgta24@gmail.com>
2019-09-12 07:33:18 +0000

    My Code

---
+++ js/index.js
@@ -0,0 +1,16 @@
+// $(document).ready(function () {
+//     var container = $('animate'),
+//         win = $(window),
+//         windowHeight = win.height();
+//     win.scroll(function () {
+//         var scrollPos = $(this).scrollTop(),
+//             containerPos = container.offset().top;
+//         if (scrollPos >= containerPos - windowHeight/1.5) {
+//             container.addClass('animated bounceInLeft');
+//         }
+//     });
+// });;
+
+//  $('animated').click(function() {
+//         $('section').addClass('animated fadeInLeft');
+//     });
