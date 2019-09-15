59ddf9ae34f6f842d71a09fc0147af953161e632
Gusvarov <nazikgta24@gmail.com>
2019-09-15 12:39:28 +0000

    My Code

--- js/index.js
+++ js/index.js
@@ -1,1 +1,20 @@
 new WOW().init();
+
+$(".confirm_password").on("keyup", function() {
+
+    const password = $(".password").val();
+    const confirmPassword = $(this).val();
+
+    if(password !=  confirmPassword) {
+
+        $(".error").html("Passwords don't match!");
+        $("#submit").attr("disabled", "disabled");
+
+    } else {
+
+        $("#submit").removeAttr("disabled");
+        $(".error").html("");
+
+    }
+
+});
