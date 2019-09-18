c272075c3d20fa6c60cd931855b4cc16bf24fd8d
Gusvarov <nazikgta24@gmail.com>
2019-09-18 14:01:25 +0000

    My Final Project

--- js/index.js
+++ js/index.js
@@ -37,3 +37,10 @@ jQuery(document).ready(function() {
   });
 
 });
+
+$(window).on('load', function () {
+    var $preloader = $('#page-preloader'),
+        $spinner   = $preloader.find('.spinner');
+    $spinner.fadeOut();
+    $preloader.delay(350).fadeOut('slow');
+});
